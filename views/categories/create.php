2657883b4cc40893909cc1c2c7f86fcf1750b5a0
Sunasuna24 <sunasunayaka1218@gmail.com>
2024-03-21 07:49:59 +0900

    カテゴリーの登録ページを作成した

---
+++ views/categories/create.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>カテゴリーを登録する | 自作の家計簿アプリ</title>
+</head>
+<body>
+    <h1>カテゴリーを登録する</h1>
+    <form action="" method="post">
+        <div>
+            <h2>カテゴリー名</h2>
+            <input type="text" name="" id="">
+        </div>
+        <div>
+            <h2>スラッグ</h2>
+            <input type="text" name="" id="">
+        </div>
+        <div>
+            <button type="submit">登録する</button>
+        </div>
+    </form>
+</body>
+</html>
